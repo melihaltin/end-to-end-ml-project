1bad6e5e121f2772f369dc6e6c746613e1192892
Melih ALTIN <melihaltindev@gmail.com>
2024-02-23 18:59:53 +0300

    data ingestion added

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from ml_project import logger
+from ml_project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("test")
+
+STAGE_NAME = "Data Ingestion"
+try:
+        logger.info(f"Starting {STAGE_NAME}")
+        obj = DataIngestionTrainingPipeline()
+        logger.info(f"{STAGE_NAME} completed")
+        
+except Exception as e:
+    logger.error(f"Failed to execute Data Ingestion stage: {str(e)}")
+    raise e 
